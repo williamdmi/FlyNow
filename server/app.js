0fac4544661e12a2fe3f49fe9783fd38c527e47c
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-15 21:21:27 +0200

    Server reading raw data

--- server/app.js
+++ server/app.js
@@ -1,17 +1,34 @@
 const cors = require("cors");
 const path = require("path");
 const express = require("express");
+const fs = require('fs');
 
 const app = express();
 const root = path.join();
 const portHttp = 4000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(root));
 
-app.listen(portHttp, async () => {
+const data = fs.readFileSync('./Raw_data/Raw_data OW.json');
+console.log(JSON.parse(data));
+
+app.listen(portHttp, async () => { 
     console.log("Hosted: http://localhost:" + portHttp);
 });
 
+app.get("/api/flight/search", async (req, res) => {
+    console.log("sending clean data");
+
+    let cleanData;
+    try {
+        
+    }
+    catch (e) {
+        console.log(e);
+    }   
+    res.json(cleanData);
+});
