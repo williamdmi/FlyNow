e64133f80fe1178a16317bbaaaa5af9fbe01815f
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-15 23:28:18 +0200

    Created data classes

---
+++ server/CleanData.js
@@ -0,0 +1,11 @@
+class CleanData {
+    constructor(ID, segments, AveragePrice, CurrencySymbol) {
+
+    }
+}
+
+class Segments {
+    constructor(legs,SegmentDuration,AirlineCode) {
+
+    }
+}
--- server/app.js
+++ server/app.js
@@ -2,19 +2,24 @@ const cors = require("cors");
 const path = require("path");
 const express = require("express");
 const fs = require('fs');
+const CleanData = require("./CleanData.js").CleanData;
 
 const app = express();
 const root = path.join();
 const portHttp = 4000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(root));
 
-const data = fs.readFileSync('./Raw_data/Raw_data OW.json');
-console.log(JSON.parse(data));
+const data = JSON.parse(fs.readFileSync('./Raw_data/Raw_data OW.json'));
+console.log(data);
+console.log(data.length);
+for(let i = 0; i <data.length; i++){
+
+}
+
 
 app.listen(portHttp, async () => { 
     console.log("Hosted: http://localhost:" + portHttp);
