2d1f4be34d645be72f7522daa3349868485eb58d
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-15 20:55:14 +0200

    Created empty server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,17 @@
 const cors = require("cors");
 const path = require("path");
 const express = require("express");
+
+const app = express();
+const root = path.join();
+const portHttp = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(root));
+
+app.listen(portHttp, async () => {
+    console.log("Hosted: http://localhost:" + portHttp);
+});
+
