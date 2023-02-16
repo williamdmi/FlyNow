5ebb4b9af2f65f8b17e0245e9224fdbbfc2d9a2f
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-17 00:12:11 +0200

    Added basic flights informations

--- client/my-app/src/Components/FlightContainer.tsx
+++ client/my-app/src/Components/FlightContainer.tsx
@@ -2,21 +2,42 @@ import React from 'react';
 import '../Styles/Main.scss';
 import '../Styles/FlightContainer.scss';
 
-class FlightContainer extends React.Component<{ flightInfo:object}, {}> {
+class FlightContainer extends React.Component<{ flightInfo:any}, {}> {
     state = {};
     async componentDidMount() {
 
+    }
+    getDeparturePointAndDate() {
+        const city = this.props.flightInfo["Segments"]["Legs"][0]["DeparturePoint"]["City"];
+        const date = this.props.flightInfo["Segments"]["Legs"][0]["DeparturePoint"]["DateTime"];
+        return(<> <div className='city'>{city}</div>
+                  <div className='date'>{date}</div> </>);
+    }
+    getDestinationAndDate() {
+        const lastLeg = this.props.flightInfo["Segments"]["Legs"].slice(-1)[0];
+        const city = lastLeg["ArrivalPoint"]["City"];
+        const date = lastLeg["ArrivalPoint"]["DateTime"];
+        return(<> <div className='city'>{city}</div>
+                  <div className='date'>{date}</div> </>);
+    }
+    getAF() {
+        const airline = this.props.flightInfo["Segments"]["Legs"][0]["AirlineName"];
+        return(<div className='airline'>{airline}</div>)
+    }
+    getPrice() {
+        const averagePrice = this.props.flightInfo["AveragePrice"];
+        return(<div className='airline'>{averagePrice}</div>)
     }
     render() {
         return (
             <table className='data-table'>
             <thead>
                 <tr>
-                    <th>tlv 23/12</th>
-                    <th>NYC בתאריך Y</th>
-                    <th>חברת תעופה AF</th>
-                    <th>מחיר לנוסע  </th>
-                    <th>הצג \ הסתר מסלול</th>
+                    <th>{this.getDeparturePointAndDate()}</th>
+                    <th>{this.getDestinationAndDate()}</th>
+                    <th>חברת תעופה{this.getAF()}</th>
+                    <th>מחיר לנוסע  {this.getPrice()}</th>
+                    <th>הצג \ הסתר מסלול{}</th>
                 </tr>
             </thead>
             <tbody>
