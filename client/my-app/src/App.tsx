d82f59f6aae9e1d6a61354c16a0399a59c14d340
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-16 21:16:44 +0200

    More style updates

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Components/Header';
+import Main from './Components/Main';
 
 function App() {
   
   return (
     <div>
         <Header />
+        <Main />
     </div>
   );
 }
---
+++ client/my-app/src/Components/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Styles/Main.scss';
+
+
+const buttons = ['socksButton', 'officersButton', 'locationsButton', 'locationsHistoryButton']
+class Main extends React.Component {
+    state = {
+        
+    };
+
+    render() {
+        return (
+            <div className="main">
+                
+            </div>
+        );
+    }
+}
+
+export default Main;
