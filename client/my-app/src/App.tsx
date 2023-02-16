d3948063f3be396a2471c386a012c20367c102b6
WilliamDmi <williamdemitrienko31@gmail.com>
2023-02-16 21:01:33 +0200

    Basic Header, Icon

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Components/Header';
 
 function App() {
   
   return (
     <div>
-
+        <Header />
     </div>
   );
 }
---
+++ client/my-app/src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/Header.scss'
+
+
+function Header() {
+  return (
+    <div className="header">
+         <h1>FlyNow</h1>
+         <img src="plane.png"></img>
+    </div>
+  );
+}
+
+export default Header;
